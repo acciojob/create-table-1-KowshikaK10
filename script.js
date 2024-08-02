62f58198d9bb93c6a1f5368951d955caa16009ee
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-08-02 09:11:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const table=document.getElementById('sampleTable');
+  let tr=`<tr><td>New Cell1</td> <td>New Cell2</td> </tr>`;
+	table.insertRow(0).innerHTML+=tr;
 }
